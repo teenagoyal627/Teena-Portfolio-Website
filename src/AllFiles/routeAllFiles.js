a77f36dd1ae9ae96754d048efa2ecdec6347bd22
teenagoyal627 <teenagoyal627@gmail.com>
2024-12-30 12:44:38 +0530

    Portfolio Website

--- src/AllFiles/routeAllFiles.js
+++ src/AllFiles/routeAllFiles.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import {Route, HashRouter as Router, Switch } from 'react-router-dom/cjs/react-router-dom.min'
-import Home from '../Components/Home/home'
-import Education from '../Components/Education/Education'
-import Experience from '../Components/Experience/Experience'
-import Skill from '../Components/Skill/Skill'
-import Project from '../Components/Project/Project'
+import React from 'react';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Home from '../Components/Home/home';
+import Education from '../Components/Education/Education';
+import Experience from '../Components/Experience/Experience';
+import Skill from '../Components/Skill/Skill';
+import Project from '../Components/Project/Project';
 
 const RouteAllFiles = () => {
   return (
-    <>
-      <Route path='/' exact><Home/></Route>
-      <Route path='/education'><Education/></Route>
-      <Route path ='/experience'><Experience/></Route>
-      <Route path='/skills'><Skill/></Route>
-      <Route path='/project'><Project/></Route>
-      </>
-    
-  )
-}
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/education" component={Education} />
+      <Route path="/experience" component={Experience} />
+      <Route path="/skills" component={Skill} />
+      <Route path="/project" component={Project} />
+    </Switch>
+  );
+};
 
-export default RouteAllFiles
+export default RouteAllFiles;
